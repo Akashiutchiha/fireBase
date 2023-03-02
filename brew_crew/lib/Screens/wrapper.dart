ee9b4f5aaaefe93335726526113d93e46ef56fce
Akashiutchiha <christianhonore2003@gmail.com>
2023-03-02 18:21:40 +0100

    lesson 3: Apps basic structure

---
+++ brew_crew/lib/Screens/wrapper.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:brew_crew/Screens/home/home.dart';
+
+class Wrapper extends StatelessWidget {
+  const Wrapper({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Home();
+  }
+}
