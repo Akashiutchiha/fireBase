0f4d5ad0b6b2241310877e53591d2e037fb2b948
Akashiutchiha <christianhonore2003@gmail.com>
2023-03-03 22:21:14 +0100

    lesson 5

--- brew_crew/lib/Screens/wrapper.dart
+++ brew_crew/lib/Screens/wrapper.dart
@@ -1,3 +1,5 @@
+import 'package:brew_crew/Screens/authenticate/authenticate.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:brew_crew/Screens/home/home.dart';
 
@@ -6,6 +8,6 @@ class Wrapper extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Home();
+    return Authenticate();
   }
 }
--- brew_crew/lib/main.dart
+++ brew_crew/lib/main.dart
@@ -1,13 +1,16 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:brew_crew/Screens/wrapper.dart';
 
-void main() {
-  runApp(const MyApp());
+Future<void> main() async {
+  //initialize firebase
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
